eba8b54d17321c562fc07b309f4bb6ba8655a55e
tdelaroche <theo.delaroche@edu.univ-fcomte.fr>
2026-03-12 18:36:08 +0100

    Added implementation for destroy and print functions

--- turtle-ast.c
+++ turtle-ast.c
@@ -122,13 +122,13 @@ struct ast_node *make_cmd_color(struct ast_node *expr) {
 
 
 void ast_node_destroy(struct ast_node *self) {
-    if (self->children_count == 0) {
-        free(self);
-        return;
-    }
     for (size_t i = 0; i < self->children_count; i++) {
         ast_node_destroy(self->children[i]);
     }
+    if (self->next != NULL) {
+        ast_node_destroy(self->next);
+    }
+    free(self);
 }
 
 void ast_destroy(struct ast *self) {
@@ -163,6 +163,102 @@ void ast_eval(const struct ast *self, struct context *ctx) {
  * print
  */
 
-void ast_print(const struct ast *self) {
+void print_ast_node(const struct ast_node *node, int indent) {
+    if (node == NULL) {
+        return;
+    }
 
+    for (int i = 0; i < indent; i++) {
+        printf(" ");
+    }
+
+    switch (node->kind) {
+        case KIND_CMD_SIMPLE:
+            printf("CMD_SIMPLE: ");
+            switch (node->u.cmd) {
+                case CMD_UP:
+                    printf("up");
+                    break;
+                case CMD_DOWN:
+                    printf("down");
+                    break;
+                case CMD_RIGHT:
+                    printf("right");
+                    break;
+                case CMD_LEFT:
+                    printf("left");
+                case CMD_HEADING:
+                    printf("heading");
+                    break;
+                case CMD_FORWARD:
+                    printf("forward");
+                    break;
+                case CMD_BACKWARD:
+                    printf("backward");
+                    break;
+                case CMD_POSITION:
+                    printf("position");
+                    break;
+                case CMD_HOME:
+                    printf("home");
+                    break;
+                case CMD_COLOR:
+                    printf("color");
+                    break;
+                case CMD_PRINT:
+                    printf("print");
+                    break;
+                default:
+                    printf("unknown command");
+                    break;
+            }
+            break;
+        case KIND_CMD_REPEAT:
+            printf("repeat:");
+            break;
+        case KIND_CMD_BLOCK:
+            printf("block:");
+            break;
+        case KIND_CMD_PROC:
+            printf("proc: %s", node->u.name);
+            break;
+        case KIND_CMD_CALL:
+            printf("call: %s", node->u.name);
+            break;
+        case KIND_CMD_SET:
+            printf("set: %s", node->u.name);
+            break;
+
+        case KIND_EXPR_VALUE:
+            printf("value: %f", node->u.value);
+            break;
+        case KIND_EXPR_UNOP:
+            printf("unop: %c", node->u.op);
+            break;
+        case KIND_EXPR_BINOP:
+            printf("binop: %c", node->u.op);
+            break;
+        case KIND_EXPR_BLOCK:
+            printf("block: %s", node->u.name);
+            break;
+        case KIND_EXPR_NAME:
+            printf("name: %s", node->u.name);
+            break;
+
+        /* Insert case KIND_EXPR_FUNC here */
+        default: break;
+    }
+    printf("\n");
+
+    for (size_t i = 0; i < node->children_count; i++) {
+        print_ast_node(node->children[i], indent + 1);
+    }
+
+    if (node->next != NULL) {
+        print_ast_node(node->next, indent);
+    }
+}
+
+void ast_print(const struct ast *self) {
+    print_ast_node(self->unit, 0);
 }
